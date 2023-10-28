2c623328e4278e844234f461b933dbe50b51552a
Nuria Ferron Ferriz <nuriaf207@gmail.com>
2023-10-28 22:05:54 +0200

    added test

--- src/ft_isalnum.c
+++ src/ft_isalnum.c
@@ -6,7 +6,7 @@
 /*   By: nuferron <nuferron@student.42barcel>       +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2023/01/11 19:23:35 by nuferron          #+#    #+#             */
-/*   Updated: 2023/09/13 20:21:41 by nuferron         ###   ########.fr       */
+/*   Updated: 2023/10/28 22:04:44 by nuferron         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -20,4 +20,42 @@ int	ft_isalnum(int c)
 		return (1);
 	else
 		return (0);
+}
+
+#include <ctype.h>
+
+int	main(void)
+{
+	int	ft;
+	int	man;
+
+	ft = ft_isalnum('a');
+	man = isalnum('a');
+	if (ft != man)
+		return (printf("%sYOU'VE FAILED TO RETURN \"a\"%s\n", RED, RESET));
+	ft = ft_isalnum('\0');
+	man = isalnum('\0');
+	if (ft != man)
+		return (printf("%sYOU'VE FAILED TO RETURN \"/\0\"%s\n", RED, RESET));
+	ft = ft_isalnum('\n');
+	man = isalnum('\n');
+	if (ft != man)
+		return (printf("%sYOU'VE FAILED TO RETURN \"/\n\"%s\n", RED, RESET));
+	ft = ft_isalnum('🏖');
+	man = isalnum('🏖');
+	if (ft != man)
+		return (printf("%sYOU'VE FAILED TO RETURN \"🏖\"%s\n", RED, RESET));
+	ft = ft_isalnum('\200');
+	man = isalnum('\200');
+	if (ft != man)
+		return (printf("%sYOU'VE FAILED TO RETURN \"/\200\"%s\n", RED, RESET));
+	ft = ft_isalnum('人');
+	man = isalnum('人');
+	if (ft != man)
+		return (printf("%sYOU'VE FAILED TO RETURN \"人\"%s\n", RED, RESET));
+	ft = ft_isalnum('∞');
+	man = isalnum('∞');
+	if (ft != man)
+		return (printf("%sYOU'VE FAILED TO RETURN \"∞\"%s\n", RED, RESET));
+	return (printf("%sYOU'VE PASSED ALL THE TESTS! 🤩\n%s", GREEN, RESET));
 }
